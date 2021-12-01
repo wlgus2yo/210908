d107020befe5401029f32c8a729df7418a39a18e
wlgus2yo <hyuni_id@kakao.com>
2021-12-01 21:49:25 +0900

    js 기본

---
+++ JS/step1/js/1_1_check.js
@@ -0,0 +1,11 @@
+function runAlert(){
+    alert("Welcome to the HELL");
+    /*알람창*/
+}
+function runLog(){
+    console.log("Welcome to the HELL!@")
+    /*콘솔창 출력*/
+}
+function runWrite(){
+    document.write("Welcome ~ js")
+}
