d050ff467aaba8d5f8bb7e739039f581758cadc7
wlgus2yo <hyuni_id@kakao.com>
2021-12-02 21:49:59 +0900

    check

--- JS/step1/js/1_1_check.js
+++ JS/step1/js/1_1_check.js
@@ -1,11 +1,23 @@
 function runAlert(){
-    alert("Welcome to the HELL");
+    alert("HELLO WORLD!");
     /*알람창*/
 }
 function runLog(){
-    console.log("Welcome to the HELL!@")
+    console.log("HELLO WORLD!@");
     /*콘솔창 출력*/
 }
 function runWrite(){
-    document.write("Welcome ~ js")
+    document.write("HELLO ~ js")
 }
+/*
+    함수 : 가장 작은 기능의 단위
+*/
+
+
+
+function calc(num1, num2){
+    var c = num1 * num2;
+    return c;
+}
+var result = calc(3, 4);
+console.log(result);
