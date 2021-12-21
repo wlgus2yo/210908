3e0e4b1b988122b0818c0e0aed51bfdf4852c30d
wlgus2yo <hyuni_id@kakao.com>
2021-12-21 09:36:33 +0900

    이미지 변경1

---
+++ jQuery/js/07_01_img.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("button:nth-of-type(1)").click(function(){
+        $("img").attr("src","images/logo_01.jpg");
+    });
+    $("button:nth-of-type(2)").click(function(){
+        $("img").attr("src","images/logo_02.jpg");
+    });
+    $("button:nth-of-type(3)").click(function(){
+        $("img").attr("src","images/logo_03.jpg");
+    });
+});
