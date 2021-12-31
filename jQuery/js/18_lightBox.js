197f6fe53a02da50dabb0675d4d661b43bbbb4cb
wlgus2yo <hyuni_id@kakao.com>
2021-12-31 22:14:26 +0900

    라이트박스 완성

--- jQuery/js/18_lightBox.js
+++ jQuery/js/18_lightBox.js
@@ -3,30 +3,41 @@
 // 이벤트 캡쳐링, 이벤트 버블링 검색하면 됩니다.
 
 $(document).ready(function(){
-    
     $("img").click(function(){//무엇을 언제?
         var $src = $(this).attr("src"); // img src값을 $src에 담는다.
-
         $("body").append("<div id='lightBox'></div>");
         $("#lightBox").append("<img src='"+$src+"'>");
         $("#lightBox").fadeIn();
-        remove();
-        stop_event();
     });
 
-    function remove(){
-        $("#lightBox").click(function(){
-            $(this).fadeOut(400);
-            setTimeout(function(){
-                $("#lightBox").remove();
-            },400);
-        });
-    }
-    function stop_event(){
-        $("#lightBox img").click(function(e){
-            e.stopPropagation();
-        });
-    }
+    $("#lightBox").click(function(){ // 이 함수가 실행되는 타이밍인 $(document).ready 타이밍에 #lightBox가 존재 하지 않는다. => 이벤트가 부착될 대상X
+        $(this).fadeOut(400); //$("#lightBox")
+        setTimeout(function(){
+            $("#lightBox").remove();
+        },400);
+    });
+
+    // $(document).on("click","대상",function(){})
+    // $("body").on("click","대상",function(){})
+    $("body").on("click","#lightBox",function(){ // body가 클릭이 됬을 때 그 시점에서 클릭 된 대상이 #lightBox를 통해 전달 됬는가를 확인 후 함수 실행
+  //$("body").on("이벤트 종류","원하는 대상",function(){})
+        $("#lightBox").fadeOut(400); //#lightBox에 사라지는 시간 초 정해주고, (0.4s, 400ms)
+        setTimeout(function(){ // n ms 후에 딱 한번만 동작하는 함수
+            $("#lightBox").remove(); // #lightBox 삭제
+        },400); // n = 400 => 400ms => 0.4s
+    });
+    $("body").on("click","#lightBox img",function(e){ 
+        // body 클릭 => 전달해준 대상이 #lightBox img가 맞는지 확인 후, 맞다면 함수 실행
+        // 클릭 된 이벤트 자체를 e라는 인자로 전달
+        e.stopPropagation();  //인자로 전달 된 a에게 이벤트 전달을 하지 않도록.
+    });
+    
+    
+    $("#linkList").on("click", "a", function (event) {
+        event.preventDefault();
+        alert("이 링크는 동작하지 않습니다!");
+    });
+
 });
 /*
     1. 라이트박스가 부드럽게 나타나도록 (페이드 인);
